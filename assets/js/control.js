d5d4ca5744aa054cffebb9b6b9252fa02111997e
faresdjefaflia <djefaflia.fares@gmail.com>
2022-03-12 20:20:20 +0100

    ADD V1.0.0-marj3i-clean-code

---
+++ assets/js/control.js
@@ -0,0 +1,20 @@
+//To add link for logo
+linkLogo = "/";
+
+
+//To add links for header
+lnkHome = "/";
+lnkServc = "/#info2";
+lnkAbout = "none";
+
+//A-SUPPORT LINK
+lnkSupport = "none";
+
+//To add links for footer
+linkFtrPrgrm = "/programming/";
+linkFtrsinfo = "none";
+linkFtrUxui = "none";
+linkFtrAdobe = "none";
+lnkPolicies = "none";
+lnkContact = "none";
+facebook = "none";
--- src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1> about </h1>
-</template>
-
-<script>
-  export default {
-    
-  }
-</script>
