2dc6b1ee20a2e72cff1748c773fbba85f553ab0c
faresdjefaflia <djefaflia.fares@gmail.com>
2022-03-16 20:13:50 +0100

    update

---
+++ components/global/Support-for-MD/programming/Web/Front/LearnBootstrap.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="box-list-crs">
+    <InfoCourse 
+      title="تعلم بوتستراب - Bootstrap"
+      info="طور مهاراتك مع افضل إطار عمل وسريع لل Html - CSS"
+    />
+    <Note>
+      بعض المحتوى هنا قد يكون قديم او بإصدارات قديمة
+    </Note>
+    <div class="all-crs">
+      <AddCourse 
+        link ="https://getbootstrap.com/docs/"
+        img="bootstrap.png"
+        title="المستندات الرسمية"
+        type="file"
+        :top="true"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PLMTdZ61eBnyrPiX-b1MNG-MSPAJ0OgpHA"
+        img="bootstrap.png"
+        title="كود شيار - دورة بوتستراب"
+        type="play"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/watch?v=rwzEb9eO8U4&t=2057s"
+        img="bootstrap.png"
+        title="بوتستراب في فيديو واحد"
+        type="play"
+      />
+      <hr>
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PLDoPjvoNmBAw24EjNUp_88S1VeaNK8Cts"
+        img="elzero.jpg"
+        title="التطبيق الاول على بوتستراب"
+        type="play"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PLDoPjvoNmBAxYmbhnyM0yxH3OzhoDiAP9"
+        img="elzero.jpg"
+        title="التطبيق الثاني على بوتستراب"
+        type="play"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PLDoPjvoNmBAy0dU3C3_lNRTSTtqePEsI2"
+        img="elzero.jpg"
+        title="التطبيق الثالث على بوتستراب"
+        type="play"
+      />
+    </div>
+    <EditBox link="/programming/Web/Front/LearnBootstrap.vue" />
+  </div>
+</template>
---
+++ components/global/Support-for-MD/programming/Web/Front/LearnFramework.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="box-list-crs">
+    <InfoCourse 
+      title="تعلم إطارات العمل"
+      info="طور مهاراتك مع افضل إطارات العمل مع الجافا سكريبت"
+    />
+    <Note>
+      يرجى الانتباه انك لست ملزم بتعلم جميع اطارات العمل، لكن يفضل البحث و الإختيار بين هذه الإطارات لذلك يستحسن مشاهدة المقدمة
+    </Note>
+    <div class="all-crs">
+      <AddCourse 
+        link ="https://www.youtube.com/watch?v=YRVavS9SzBw"
+        img="elzero.jpg"
+        title="الزيرو - مقدمة وتعريف"
+        type="play"
+        :top="true"
+      />
+      <hr>
+      <AddCourse 
+        link ="https://ar.reactjs.org/docs/getting-started.html"
+        img="react.svg"
+        title="المستندات الرسمية لـ react"
+        type="file"
+        :top="true"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PL1FWK-sgJ9el-axKTMU_1l5PEyv7tn-wk"
+        img="react.svg"
+        title="دورة رياكت - react"
+        type="play"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/watch?v=QDGe4emCPG0&t=236s"
+        img="react.svg"
+        title="تعلم رياكت بفيديو واحد"
+        type="play"
+      />
+      <hr>
+      <AddCourse 
+        link ="https://vuejs.org/guide/"
+        img="vue.svg"
+        title="المستندات الرسمية لـ Vue.js"
+        type="file"
+        :top="true"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/playlist?list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f"
+        img="vue.svg"
+        title=" دورة Vue.js"
+        type="play"
+      />
+      <hr>
+      <AddCourse 
+        link ="https://angular.io/docs"
+        img="angular.png"
+        title="المستندات الرسمية لـ angular"
+        type="file"
+        :top="true"
+      />
+      <AddCourse 
+        link ="https://www.youtube.com/results?search_query=%D8%AF%D9%88%D8%B1%D8%A9+angular&sp=EgIQAw%253D%253D"
+        img="angular.png"
+        title="يوتيوب - دورات angular"
+        type="play"
+      />
+    </div>
+    <EditBox link="/programming/Web/Front/LearnFramework.vue" />
+  </div>
+</template>
